a2b6a8ce9b5393de3f6e22dfc4b88682e91ac461
Roy <b.roynaim@gmail.com>
2021-03-05 17:10:00 +0100

    fizzbuzz printed

--- main.js
+++ main.js
@@ -4,5 +4,37 @@
 
 for (var i = 1; i <= 100; i++) {
   var numbers = i;
+  // console.log(numbers);
+  if (numbers%3 === 0 && numbers%5 ===0) {
+    var numbers = 'FizzBuzz';
+  }
+
+  if (numbers%3 ===0) {
+    var numbers = 'Fizz';
+  }
+
+  if (numbers%5 ===0) {
+    var numbers = 'Buzz';
+  }
   console.log(numbers);
 }
+
+//solution found from https://forum.freecodecamp.org/t/problem-1-multiples-of-3-and-5/206277
+  // if((i % 3 === 0 )||(i % 5 === 0)||(i % 3 === 0 && i % 5 === 0)){
+  //   numbers = numbers + i;
+  //   console.log(numbers);
+  // }
+
+
+//soluzione alternativa
+
+//   if (i%3 === 0 && i%5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i%3 === 0) {
+//     console.log('Fizz');
+//   } else if (i%5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(numbers);
+//   }
+// }
